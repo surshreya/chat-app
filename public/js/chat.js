ccd88c17bd0c4d9e56022f5e08f45697b24eaf45
surshreya <surshreya.0727@gmail.com>
2023-01-21 09:50:18 +0530

    establishing data passing b/w server and client

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,28 @@
+const body = document.querySelector("body");
 /**
  * Connect to the web socket from the client side
  */
-io();
+const socket = io();
+
+// Get data from server
+socket.on("count", (count) => {
+  console.log(count);
+});
+
+socket.on("message", (msg) => {
+  //   const welcomeHTML = `<h3>${msg}</h3>`;
+  //   body.insertAdjacentHTML("afterbegin", welcomeHTML);
+  console.log(msg);
+});
+
+const btnIncrement = document.querySelector(".increment");
+btnIncrement.addEventListener("click", (e) => {
+  // Pass client data to server
+  socket.emit("increment");
+});
+
+document.getElementById("message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = document.querySelector("input").value;
+  socket.emit("sendMessage", message);
+});
--- src/index.js
+++ src/index.js
@@ -8,8 +8,23 @@ const io = socketio(server); // Support Web Sockets
 const port = process.env.PORT || 3000;
 
 // Set up Server side web socket connection
-io.on("connection", () => {
+let count = 0;
+io.on("connection", (socket) => {
   console.log("New connection...");
+
+  // Pass data from server to client
+  socket.emit("count", count);
+  socket.emit("message", "Welcome");
+
+  //Get data from client
+  socket.on("increment", () => {
+    count++;
+    io.emit("count", count); // Notify all the clients
+  });
+
+  socket.on("sendMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 server.listen(port, () => {
